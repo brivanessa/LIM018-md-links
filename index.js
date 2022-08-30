e65b6a238944b75a432dec11aa65431930b635c6
Vanessa <vanessa@MacBook-Air-de-Vanessa.local>
2022-08-30 12:14:15 -0500

    usando metodo fs con node

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 module.exports = () => {
   // ...
 };
+
+// EJEMPLOS
+console.log('hola :)') // node .
+const pets = ['🐶', '🐱', '🐰'];
+const nickName = [10,20,30];
+
+module.exports = {
+    pets,
+    nickName,
+};
