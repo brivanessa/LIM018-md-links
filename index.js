b86931a8afd1e9fbbdf19ff13420c4b9f4f3908c
brivanessa <vanessaah.lu@gmail.com>
2022-09-06 07:20:32 -0500

    mdLinks with colors

--- index.js
+++ index.js
@@ -1,44 +1,56 @@
 //instalar modulos
 const fs = require('fs'); // Se importa en una var fs mediante require el modulo file_system 
 // En consola : npm install  marked 
-const marked = require('marked');
-const cheerio = require('cheerio');
-const axios = require('axios');
+const marked = require('marked'); //HTML
+const cheerio = require('cheerio'); //ELEMENT
+const axios = require('axios'); //BREAK
+//format
+const colors = require('colors');
 
+
+const principal =()=>{
+    console.log(`***************************************************************`.yellow)
+    console.log(`                              mdLinks :)                       `)
+    console.log(`***************************************************************`.yellow)
+}
 // MD LINKS LISTA DE LINKS
 const mdLinks = (document) => {
+console.log(`LINKS FOUND`.yellow)
 fs.readFile(document,'utf-8',(error,data)=>{
 if (!error){
     let mdToHtml=cheerio.load(marked.parse(`'# Marked in Node.js\n\nRendered by **${data}**.`));
     mdToHtml('a').each(function(indice, elemento){
-        console.log(`${indice}  ${elemento.children[0].nodeValue} ${elemento.attributes[0].value}`)
+        const numberLink =`${indice}`.yellow;
+        console.log(` LINK Nº ${numberLink}: ${elemento.children[0].nodeValue} ${elemento.attributes[0].value}`)
     })
 } else {
     console.log(`Error: ${error}`)
 }
 })
 }
-mdLinks('readmeExample.md');
+// mdLinks('readmeExample.md');
 
 // MD LINKS STATUS CODE
 const mdLinks2 = (document) => {
+
     fs.readFile(document,'utf-8',(error,data)=>{
     if (!error){
+        console.log(`LINKS STATE`.yellow)
         let mdToHtml=cheerio.load(marked.parse(`'# Marked in Node.js\n\nRendered by **${data}**.`));
         mdToHtml('a').each(function(indice, elemento){
             axios.get(`${elemento.attributes[0].value}`)
             .then((response)=>{
-                console.log(`${indice}  ${elemento.children[0].nodeValue} ${elemento.attributes[0].value}
-                ${response.status} ${response.statusText}`)
+                const oks = `${response.statusText}`.green;
+                console.log(` ${indice} ${oks} (${response.status}): ${elemento.children[0].nodeValue} -> ${elemento.attributes[0].value}`)
             })
             .catch((error)=>{
                 let errorName=`'${error}'`;
                 if (error==`AxiosError: Request failed with status code 404`){
-                console.log(`${indice}  ${elemento.children[0].nodeValue} ${elemento.attributes[0].value}
-                404 FAIL`) 
+                const fail = `FAIL`.bgYellow;    
+                console.log(` ${indice} ${fail} (404): ${elemento.children[0].nodeValue} -> ${elemento.attributes[0].value}`) 
                 } else if (errorName.includes('ENOTFOUND')== true){
-                    console.log(`${indice}  ${elemento.children[0].nodeValue} ${elemento.attributes[0].value}
-                not found page FAIL`) 
+                const fail = `FAIL`.bgYellow;     
+                console.log(` ${indice} ${fail} (not found page): ${elemento.children[0].nodeValue} -> ${elemento.attributes[0].value} `)
                 } else {console.log(`${indice}  ${elemento.children[0].nodeValue} ${elemento.attributes[0].value}
                 ${error}`)}    
             })
@@ -48,7 +60,10 @@ const mdLinks2 = (document) => {
     }
     })
     }
-mdLinks2('readmeExample.md');
+
+principal();
+mdLinks('readmeExample2.md');   
+mdLinks2('readmeExample2.md');
 
 
 module.exports = () => {
--- readmeExample.md
+++ readmeExample.md
@@ -16,5 +16,5 @@ Dentro de una comunidad de código abierto, nos han propuesto crear una
 herramienta usando [Node.js](http://nodejs.og/), que lea y analice archivos
 en formato `Markdown`, para verificar los links que contengan y reportar
 algunas estadísticas. [PIXAR](https://www.lego.com/en-us/notfound)
-
+  * [Arreglos](https://blueg.co.uk/404)
 ![md-links](https://user-images.githubusercontent.com/110297/42118443-b7a5f1f0-7bc8-11e8-96ad-9cc5593715a6.jpg)
