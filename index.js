3aa6f31c59224b8cd26a6ab2fc3bc3ad11ef04c4
brivanessa <vanessaah.lu@gmail.com>
2022-09-06 05:36:12 -0500

    funciones mdLink

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const fs = require('fs'); // Se importa en una var fs mediante require el modulo
 // En consola : npm install  marked 
 const marked = require('marked');
 const cheerio = require('cheerio');
+const axios = require('axios');
 
-const mdLinks = () => {
-console.log('hola mundo')
-fs.readFile('readmeExample.md','utf-8',(error,data)=>{
+// MD LINKS LISTA DE LINKS
+const mdLinks = (document) => {
+fs.readFile(document,'utf-8',(error,data)=>{
 if (!error){
     let mdToHtml=cheerio.load(marked.parse(`'# Marked in Node.js\n\nRendered by **${data}**.`));
     mdToHtml('a').each(function(indice, elemento){
@@ -17,8 +18,38 @@ if (!error){
 }
 })
 }
+mdLinks('readmeExample.md');
 
-mdLinks();
+// MD LINKS STATUS CODE
+const mdLinks2 = (document) => {
+    fs.readFile(document,'utf-8',(error,data)=>{
+    if (!error){
+        let mdToHtml=cheerio.load(marked.parse(`'# Marked in Node.js\n\nRendered by **${data}**.`));
+        mdToHtml('a').each(function(indice, elemento){
+            axios.get(`${elemento.attributes[0].value}`)
+            .then((response)=>{
+                console.log(`${indice}  ${elemento.children[0].nodeValue} ${elemento.attributes[0].value}
+                ${response.status} ${response.statusText}`)
+            })
+            .catch((error)=>{
+                let errorName=`'${error}'`;
+                if (error==`AxiosError: Request failed with status code 404`){
+                console.log(`${indice}  ${elemento.children[0].nodeValue} ${elemento.attributes[0].value}
+                404 FAIL`) 
+                } else if (errorName.includes('ENOTFOUND')== true){
+                    console.log(`${indice}  ${elemento.children[0].nodeValue} ${elemento.attributes[0].value}
+                not found page FAIL`) 
+                } else {console.log(`${indice}  ${elemento.children[0].nodeValue} ${elemento.attributes[0].value}
+                ${error}`)}    
+            })
+        })
+    } else {
+        console.log(`Error: ${error}`)
+    }
+    })
+    }
+mdLinks2('readmeExample.md');
+
 
 module.exports = () => {
     mdLinks
