42fa933aa06b5a766a97b5dd0313d4cd3133a082
brivanessa <vanessaah.lu@gmail.com>
2022-09-06 01:24:12 -0500

    links por archivo

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
+//instalar modulos
+const fs = require('fs'); // Se importa en una var fs mediante require el modulo file_system 
+// En consola : npm install  marked 
+const marked = require('marked');
+const cheerio = require('cheerio');
+
+const mdLinks = () => {
+console.log('hola mundo')
+fs.readFile('readmeExample.md','utf-8',(error,data)=>{
+if (!error){
+    let mdToHtml=cheerio.load(marked.parse(`'# Marked in Node.js\n\nRendered by **${data}**.`));
+    mdToHtml('a').each(function(indice, elemento){
+        console.log(`${indice}  ${elemento.children[0].nodeValue} ${elemento.attributes[0].value}`)
+    })
+} else {
+    console.log(`Error: ${error}`)
+}
+})
+}
+
+mdLinks();
+
 module.exports = () => {
-  // ...
-};
-
-// EJEMPLOS
-console.log('hola :)') // node .
-const pets = ['🐶', '🐱', '🐰'];
-const nickName = [10,20,30];
-
-module.exports = {
-    pets,
-    nickName,
-};
+    mdLinks
+  };
+  
--- prueba.js
+++ prueba.js
@@ -1,60 +1,64 @@
-const {pets, nickName } = require('./index');
-pets.forEach((item) => console.log(item));
-console.log(nickName);  // node . // node index
+// const {pets, nickName } = require('./index');
+// pets.forEach((item) => console.log(item));
+// console.log(nickName);  // node . // node index
 
-// probando modulo file_system 
+// //instalar modulos
+// // probando modulo file_system 
+// const fs = require('fs'); // importo en una var fs mediante require el modulo fs
 
-const fs = require('fs'); // importo en una var fs mediante require el modulo fs
+// //WRITEFILE - CREAR UN ARCHIVO Y SU CONTENIDO 
+// fs.writeFile('data1.txt','hola bri :) vane', (error)=>{
+//     if (error){
+//         console.log(`Error: ${error}`)
+//     }
+// }) //1er parametro doc, lo que se escribira en el doc, fn anonima para controlar casos de error
 
-//WRITEFILE - CREAR UN ARCHIVO Y SU CONTENIDO 
-fs.writeFile('data1.txt','hola bri :) vane', (error)=>{
-    if (error){
-        console.log(`Error: ${error}`)
-    }
-}) //1er parametro doc, lo que se escribira en el doc, fn anonima para controlar casos de error
+// // READFILE - LEER UN ARCHIVO EN LA CONSOLA
+// fs.readFile('data1.txt','utf-8',(error,data)=>{
+// if (!error){
+//     console.log(data);
+// } else {
+//     console.log(`Error: ${error}`)
+// }
+// })
 
-// READFILE - LEER UN ARCHIVO EN LA CONSOLA
-fs.readFile('data1.txt','utf-8',(error,data)=>{
-if (!error){
-    console.log(data);
-} else {
-    console.log(`Error: ${error}`)
-}
-})
+// //RENAME ARCHIVO
+// fs.rename('data1.txt', 'dataEjem.txt',(error)=>{
+//     if (!error){
+//         console.log('se renombró');
+//     } else{
+//         console.log(`Error:${error}`);
+//     }
+// })
 
-//RENAME ARCHIVO
-fs.rename('data1.txt', 'dataEjem.txt',(error)=>{
-    if (!error){
-        console.log('se renombró');
-    } else{
-        console.log(`Error:${error}`);
-    }
-})
+// //AÑADIR CONTENIDO
+// fs.appendFile('dataEjem.txt','Hola vane',(error)=>{
+//     if(!error){
+//         console.log('se agregó correctamente');
+//     } else {
+//         console.log(`Error:${error}`);
+//     }
+// })
 
-//AÑADIR CONTENIDO
-fs.appendFile('dataEjem.txt','Hola vane',(error)=>{
-    if(!error){
-        console.log('se agregó correctamente');
-    } else {
-        console.log(`Error:${error}`);
-    }
-})
+// //PARA CREAR UNA COPIA
+// fs.createReadStream('dataEjem.txt').pipe(fs.createWriteStream('dataEjem_copia.txt')) //la fn -metodo pipe me permite hacer una copia
 
-//PARA CREAR UNA COPIA
-fs.createReadStream('dataEjem.txt').pipe(fs.createWriteStream('dataEjem_copia.txt')) //la fn -metodo pipe me permite hacer una copia
+// //PARA ELIMINAR ARCHIVO
+// fs.unlink('dataEjem_copia.txt', error =>{
+//     if(error){
+//         console.log(`Error:${error}`);
+//     }
+// })
 
-//PARA ELIMINAR ARCHIVO
-fs.unlink('dataEjem_copia.txt', error =>{
-    if(error){
-        console.log(`Error:${error}`);
-    }
-})
+// //PARA LEER ARCHIVOS DE UN DIRECTORIO
+// fs.readdir('./',(error,docs)=>{
+//     if(!error){
+//         docs.forEach(doc => {
+//             console.log(doc)
+//         })
+//     }
+// })
 
-//PARA LEER ARCHIVOS DE UN DIRECTORIO
-fs.readdir('./',(error,docs)=>{
-    if(!error){
-        docs.forEach(doc => {
-            console.log(doc)
-        })
-    }
-})
+// // PARA INSTALAR LOS MODULOS
+// // (CTRL + L = clear)
+// // 
