a2bcc76438d867ed4f8951ca387483affcade3d0
brivanessa <vanessaah.lu@gmail.com>
2022-09-15 13:11:15 -0500

    probando array de links

--- index.js
+++ index.js
@@ -112,46 +112,95 @@ const readmdLinks=(document) => {
 
 // MD LINKS STATUS CODE     *************************************************************
 let docsLinkStatus=[];
-
-const readmdLinkStatus= (document) => {
+let docsLinkStatusLink=[];
+const readmdLinksGlobal= (document) => {
     return new Promise ((res,rej)=>{
         if (fs.readFileSync(document,'utf-8')===''){
             rej('el archivo esta vacio')
         } else {
             let data = fs.readFileSync(document,'utf-8')
             let mdToHtml=cheerio.load(marked.parse(`'# Marked in Node.js\n\nRendered by **${data}**.`));
-         mdToHtml('a').each(  function(indice,elemento){
-         axios.get(`${elemento.attributes[0].value}`)
-            .then( (response)=>{
-                (docsLinkStatus.push({
-                    'href': `${elemento.attributes[0].value}`,
-                    'text': `${elemento.children[0].nodeValue}`,
-                    'file': `${path.basename(document)}`,
-                    'route': `${pathGlobal(document)}`,
-                    'status': `${ response.status}`,
-                    'result':  `${response.statusText}`,
-                  }))
-            })
-            .catch((error)=>{
-                return(docsLinkStatus.push({
-                    'href': `${elemento.attributes[0].value}`,
-                    'text': `${elemento.children[0].nodeValue}`,
-                    'file': `${path.basename(document)}`,
-                    'route': `${pathGlobal(document)}`,
-                    'status': `${error}`,
-                    'result':  `FAIL`,
-                })) 
-            })
-            res( docsLinkStatus)
+         mdToHtml('a').each( function(indice,elemento){
+            docsLinkStatusLink.push(elemento.attributes[0].value) 
+            res(docsLinkStatusLink)
              })
         }
     })
 }
-//console.log(readmdLinkStatus('readmeExample.md'));
+//console.log(readmdLinksGlobal('readmeExample.md'));
 
+
+const readmdLinkStatus= async(document) => {
+    let linksArray = await (readmdLinksGlobal(document))
+    //console.log(linksArray)
+    linksArray.map((link)=> {
+        axios.get(`${link}`)
+        .then( (response)=>{
+            return (docsLinkStatus.push({
+                'href': `${link}`,
+               // 'text': `${elemento.children[0].nodeValue}`,
+                'file': `${path.basename(document)}`,
+                'route': `${pathGlobal(document)}`,
+                'status': `${ response.status}`,
+                'result':  `${response.statusText}`,
+            }))
+         })
+        .catch((error)=>{
+            return (docsLinkStatus.push({
+                'href': `${link}`,
+               // 'text': `${elemento.children[0].nodeValue}`,
+                'file': `${path.basename(document)}`,
+                'route': `${pathGlobal(document)}`,
+                'status': `${error}`,
+                'result':  `FAIL`,
+            })) 
+        })
+        return (docsLinkStatus) 
+         })
+}
+
+readmdLinkStatus('readmeExample.md');
+//console.log(readmdLinkStatus('readmeExample.md'))
+setTimeout(()=>{console.log(docsLinkStatus)},3000)
+
+
+
+
+// const readmdLinkStatus= (document) => {
+//   return new Promise ((res,rej)=>{
+//     (readmdLinksGlobal(document))
+//     .then(data =>{return readmdLinks(data)})
+//     .catch(error => console.log(error))
+//     docsLinkStatusLink.map((link)=> {
+//             axios.get(`${link}`)
+//             .then( (response)=>{
+//                 return (docsLinkStatus.push({
+//                     'href': `${link}`,
+//                 // 'text': `${elemento.children[0].nodeValue}`,
+//                     //'file': `${path.basename(document)}`,
+//                     'route': `${pathGlobal(document)}`,
+//                     'status': `${ response.status}`,
+//                     'result':  `${response.statusText}`,
+//                 }))
+//             })
+//             .catch((error)=>{
+//                 return (docsLinkStatus.push({
+//                     'href': `${link}`,
+//                 // 'text': `${elemento.children[0].nodeValue}`,
+//                    // 'file': `${path.basename(document)}`,
+//                     'route': `${pathGlobal(document)}`,
+//                     'status': `${error}`,
+//                     'result':  `FAIL`,
+//                 })) 
+//             })
+//             return (docsLinkStatus) 
+//         })
+//         res(docsLinkStatus) 
+//     })
+// }
 // readmdLinkStatus('readmeExample.md');
 // // //console.log(readmdLinkStatus('readmeExample.md'))
-// setTimeout(()=>{console.log(docsLinkStatus)},3000)
+//setTimeout(()=>{console.log(docsLinkStatus)},3000)
 
 // CREANDO PROMESAS => MDLINKS *************************************************************
 const mdLinks = (route,elements) => {
