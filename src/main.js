c78c1fcfbde41a08853eea85cd493e52d36de2c5
brivanessa <vanessaah.lu@gmail.com>
2022-09-20 23:18:59 -0500

    main sin comments

--- src/main.js
+++ src/main.js
@@ -5,10 +5,9 @@ const path = require('path');
 const marked = require('marked'); //HTML // En consola : npm install  marked 
 const cheerio = require('cheerio'); //ELEMENT
 const axios = require('axios'); //BREAK
-const colors = require('colors');
+
 // LA RUTA EXISTE          YES TEST *****************************************
 const existRoute =(route) => fs.existsSync((route))
-//console.log(existRoute('../carpeta'))
 // RUTA ABSOLUTA Y NORMALIZADA    YES TEST **********************************
 function pathGlobal(routaInput){
     return path.normalize(path.isAbsolute(routaInput)?routaInput:path.resolve(routaInput));
@@ -19,7 +18,6 @@ let error1 = 'no hay archivos en la carpeta'
 let error2 = 'la carpeta o ruta no existen'
 let error3 = 'no hay archivos .md en la carpeta'
 let error4 = 'no hay folders en la carpeta'
-
 //          PASO 1: LISTA DE ARCHIVOS DE UN DIRECTORIO ************************
 function readFolder(ruta){
     let rutaAbsoluta = pathGlobal(ruta);
@@ -62,12 +60,6 @@ const  pathRead = (ruta) => {
             return(docslist);
         } 
     }
-/******** PROBANDO pathRead
-//console.log(pathRead('../carpeta'))
-//console.log(pathRead('../folderFiles0'))
-//console.log(pathRead('../folderFilesNoMd'))
-//console.log(pathRead('../folderTestOneFileMd'))
-*/
 // LEER ARCHIVOS ARCHIVOS MD           YES TEST*******************************
 const pathReadFile = (ruta)=> { 
         if(path.extname(ruta)==='.md'){
@@ -75,10 +67,6 @@ const pathReadFile = (ruta)=> {
             return(pathGlobal(ruta))
         } else {return('no es un archivo .md...')}  
 }
-/******** PROBANDO pathReadFile 
-console.log(pathReadFile('../readmeExample.md'))
-//console.log(pathReadFile('../thumb.png'))
-*/
 // MD LINKS LISTA DE LINKS ***YES TEST (CAMBIAR fs.Sync)***********************
 let doclistLinks=[];
 const readmdLinks=(document) => {
@@ -101,19 +89,12 @@ const readmdLinks=(document) => {
         }
     })
 }
-/******** PROBANDO eadmdLinks 
-console.log(readmdLinks('../readmeExample.md'));
-*/
 // MD LINKS STATS LISTA DE LINKS CON PROMESAS *******************************
 let linksGlobal=[];
-let leerDocs=[]
 /*---------------------- PASO 1: LEER .MD            YES TEST **************/
 const readDocuments = (document) => { 
     return fs.promises.readFile(document,'utf-8')
-    .then(data => { 
-        //leerDocs.push([document,data])
-        return([document,data])
-    }) 
+    .then(data  =>{ return([document,data])}) 
     .catch(error=>{
         return((`${error.code}`=='ENOENT')?(`${error.code}: el archivo no existe`):`${error.code}`)
     })
@@ -123,43 +104,13 @@ const readDocumentsArr = (documentArr) => {
     return Promise.all(
         documentArr.map((document)=>{
                 return readDocuments(document)
-                .then(data => { 
-                    return data
-                })
+                .then(data => { return data})
                 // .catch(error=>{  // ES NECESARIO?
                 //     return error
                 // }) 
-            })
+        })
     )              
-}
-/*
-let arrrayEjemplo= [
-    '/Users/vanessa/Documents/LABORATORIA_018_2022/4_Proyecto/LIM018-md-links/carpeta/readmeExample.md',
-    '/Users/vanessa/Documents/LABORATORIA_018_2022/4_Proyecto/LIM018-md-links/carpeta/carpeta1/aeadmExample.md',
-    '/Users/vanessa/Documents/LABORATORIA_018_2022/4_Proyecto/LIM018-md-links/carpeta/carpeta1/readmeExample2.md',
-    '/Users/vanessa/Documents/LABORATORIA_018_2022/4_Proyecto/LIM018-md-links/carpeta/carpeta1/fs/ffs/readmeExample.md'
-  ]
-//readDocumentsArr(arrrayEjemplo) 
-// .then((data)=> {console.log('aqui',data)})
-// .catch((error)=> {return (error)}) 
-//console.log('aqui',readDocumentsArr(arrrayEjemplo) )
-readDocumentsArr(arrrayEjemplo)
-.then(data =>{
-    return data.map((item)=>{
-        return converMdToHtml(item)
-    })
-})
-.then(data =>{
-    //console.log(readmdLinkStatus(data[0]))
-    //return(readmdLinkStatus(data[0]))
-    return(readmdLinkStatus(...new Set(data)))
-    })
-
-.then(data =>{
-        console.log(data)
-        return(data)
-    })
- */   
+}  
 /*---------------------- PASO 2: convertir Md a HTML       YES TEST **************/
 const converMdToHtml = (documentHtml) => {
         if(documentHtml!=''){
@@ -170,19 +121,6 @@ const converMdToHtml = (documentHtml) => {
             return(linksGlobal)
         } else {return('no hay links...')}
 }
-// const converMdToHtml = (documentHtml) => {
-//     if(documentHtml!=''){
-//         let mdToHtml=cheerio.load(marked.parse(`'# Marked in Node.js\n\nRendered by **${documentHtml[1]}**.`));
-//         mdToHtml('a').each( function(indice,elemento){
-//             return linksGlobal.push([documentHtml[0],elemento.children[0].nodeValue,elemento.attributes[0].value])  
-//         })
-//         return(linksGlobal)
-//     } else {return('no hay links...')}
-// }
-/******** PROBANDO converMdToHtml 
-console.log(converMdToHtml(['../readmeAllOkLinks.md','[Node.js](http://nodejs.og/)']))
-////result:[ [ '../readmeAllOkLinks.md', 'Node.js', 'http://nodejs.og/' ] ]
-*/
 /*---------------------- PASO3: STATS DE LINKS       DE DIFERENTES .MD    YES TEST **************/
 let statLinks;
 const statsArrayGlobal= (arrayLinks) => {
@@ -195,29 +133,6 @@ const statsArrayGlobal= (arrayLinks) => {
     }
     else{return('...no se puede analizar')}
 }
-
-/******** PROBANDO statsArrayGlobal 
-console.log(statsArrayGlobal([[ '../readmeAllOkLinks.md', 'N.js', 'https://es.wikipedia.org/wiki/Markdown'],
-    [ '../readmeAllOnks.md', 'No.js','https://es.wikipedia.org/wiki/Markdown'],
-    [ '../readmeAs.md', 'Nod.js','https://www.kualo.co.uk/404']]))
-////result: { Total: '3', Unique: '2' }
-*/
-
-/* PROBANDO statsArrayGlobal CUANDO NOS DAN ARCHIVO .MD
-//------------------------------------------------------
-readDocuments('../readmeAllOkLinks.md')
-//readDocuments('../readmeVaciodd.txt')
-//readDocuments('')
-//readDocuments('/Users/vanessa/Documents/LABORATORIA_018_2022/4_Proyecto/LIM018-md-links/readmeExample.md')
-        .then(data => { 
-            let linksDelMd=converMdToHtml(data)
-            console.log(statsArrayGlobal(linksDelMd))
-            return (statsArrayGlobal(linksDelMd))
-        }) 
-        .catch(error=>{ return error })    
-
-*/
-
 /*---------------------- PASO3: STATS DE LINKS  CON STATUS            YES TEST **************/
 const statsArrayStatus= (arrayLinks) => {
     if(typeof(arrayLinks)=='object'&& arrayLinks.length>=1){
@@ -243,31 +158,6 @@ const statsArrayStatus= (arrayLinks) => {
     }
     else{return('No hay links por analizar...')}
 }
-/*
-let ejemplo =[
-    {
-      file: '/Users/',
-      href: 'https://es.wikipedia.org/wiki/Markdown',
-      result: 'OK'
-    },
-    {
-      file: '/Users/',
-      href: 'https://es.wikipedia.org/wiki/Markdown',
-      result: 'OK'
-    },
-    {
-        file: '/Users/',
-        href: 'https://es.wikipedia.org/wiki/Markdo',
-        result: 'OK'
-    },
-    {
-        file: '/Users/',
-        href: 'https://es.wikipedia.org/wiki/Markdown',
-        result: 'FAIL'
-    },
-]  
-console.log(statsArrayStatus(ejemplo))
-*/
 // MD LINKS STATUS CODE     *************************************************************
 let docsLinkStatusOk;
 let docsLinkStatusFail; 
@@ -315,58 +205,7 @@ const readmdLinkStatus = (linksGlobal) => {
                 } 
             }); 
     }))
-} 
-
-/******** PROBANDO readmdLinkStatus 
-let linksExamples= [
-    [ '../readmeAllOkLinks.md', 'N.js', 'https://es.wikipedia.org/wiki/Markdown'],
-    [ '../readmeAllOnks.md', 'No.js','https://es.wikipedia.org/wiki/Markdown'],
-    [ '../readmeAs.md', 'Nod.js','https://www.kualo.co.uk/404'],
-    [ '../readmeAllOkLinkd', 'Node.js','https://www.kualo.co.uk/404'],
-    [ '../readmeAllOkLinkmd', 'Nodes.js','http://nodejs.org/'],
-    [ '../readmeAllmd', 'Nodesp.js','https://blueg.co.uk/404'],
-    ]
-readmdLinkStatus(linksExamples)  
-.then((data) => {
-    console.log(data)
-    return data
-})
-.catch((error) => {
-    console.log(error)
-    return error
-})
-*/ 
-
-/* PROBANDO readmdLinkStatus CUANDO NOS DAN ARCHIVO .MD
-//------------------------------------------------------
-const readmdLinkdeMd = (document) => {
-    return readDocuments(document)
-        .then(data => { 
-            return converMdToHtml(data)
-        })
-        .catch(error=>{ return error }) 
-        .then(data => { 
-            return readmdLinkStatus(data)
-        })
-        .catch(error=>{ return error })    
-    }
-
-//readmdLinkdeMd('../readmeAllOkLinks.md')
-//readmdLinkdeMd('../readmeVaciodd.txt')
-//readmdLinkdeMd('')
-//readmdLinkdeMd('/Users/vanessa/Documents/LABORATORIA_018_2022/4_Proyecto/LIM018-md-links/readmeExample.md')
-readmdLinkdeMd('../readmeExample.md')
-    .then(data=>{
-        console.log(data)
-       //console.log(docsLinkStatusOk)
-        ////return(data)   
-    })
-    .catch(error=>{
-       console.log(error)
-       //console.log(docsLinkStatusFail)
-       //// return(error)
-    })
-//*/
+}  
 
 /*---------------------- PASO3: STATS DE LINKS DE UN ARCHIVO MD      **************/
 let statLinksLast;
@@ -379,13 +218,7 @@ const statsArray= (arrayLinks) => {
     }
     else{return('...no se puede analizar')}
 }
-/******** PROBANDO statsArrayGlobal 
-console.log(statsArray(['https://es.wikipedia.org/wiki/Markdown','https://es.wikipedia.org/wiki/Markdown','https://www.kualo.co.uk/404']))
-////result: { Total: '3', Unique: '2' }
-//*/
-
 module.exports = {
-    //mdLinks,
     existRoute,
     pathGlobal,
     pathReadFile,
@@ -405,6 +238,5 @@ module.exports = {
     statLinks,
     docsLinkStatusOk,
     docsLinkStatusFail,
-    //functionGlobalStats,
   };
   
